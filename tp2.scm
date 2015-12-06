e5be5907f57ba82630c5526f6a6c5ab60580c8a4
Sulliman Aiad <info@sullimanaiad.com>
2015-12-06 22:40:28 +0100

    Final version (debugged and everything, I think).

--- tp2.scm
+++ tp2.scm
@@ -30,47 +30,65 @@
   (lambda (lst pred)
     (let struct ((fini '()) 
                  (restant lst)
-                 (enCours '()))
+                 (en-cours '()))
     
       (if (null? restant)
-        (if (null? enCours)
+        (if (null? en-cours)
           fini
           
-          (reverse (cons (reverse enCours) fini)))
+          (reverse (cons (reverse en-cours) fini)))
           
         (if (pred (car restant))
-          (struct (if (null? enCours)
+          (struct (if (null? en-cours)
                     fini
-                    (cons (reverse enCours) fini))
+                    (cons (reverse en-cours) fini))
                   (cdr restant)
                   '())
           
           (struct fini
                   (cdr restant)
-                  (cons (car restant) enCours)))))))
+                  (cons (car restant) en-cours)))))))
+
+; Permet de retirer une variable du dictionnaire.
+; Inspiré par : http://stackoverflow.com/a/4452203/5354535
+(define delete-var
+  (lambda (var dict)
+    (let ((couple (assoc var dict)))
+      (cond
+        ((null? dict) dict)
+        ((eq? couple (car dict)) (delete-var var (cdr dict)))
+        (else (cons (car dict) (delete-var var (cdr dict))))))))
+
+; Permet d'affecter une valeur à une variable dans le dictionnaire (ajout ou modification).
+(define affect-var
+  (lambda (dict var value)
+    (if (assoc var dict)
+      ; La variable existe déjà, on reconstruit sans, et on rajoute.
+      (affect-var (delete-var var dict) var value)
+      
+      ; La variable n'existe pas encore, on rajoute.
+      (append dict (list (list var value))))))
 
 ; Géante fonction récursive qui traite tous les cas de figure et retourne la réponse à une commande.
 (define process
-  (lambda (input stack)
-    (if (= (length input) 0)
+  (lambda (input stack dict output?)
+    (let ((return (if (= (length input) 0)
       ; S'il n'y a plus rien comme commandes.
       (if (= (length stack) 0)
         ; Et si le stack est vide, retourner une erreur.
-        (string->list "Veuillez entrer une commande.")
+        (cons (string->list "Veuillez entrer une commande.") dict)
         
         (if (= (length stack) 1)
           ; Sinon, si le stack ne contient que le résultat final.
-          (string->list (number->string (car (reverse stack))))
+          (cons (string->list (number->string (car (reverse stack)))) dict)
           
           ; Si le stack contient trop d'éléments (> 1).
-          (string->list "Erreur de syntaxe: veuillez revoir votre commande.")
-        )
-      )
+          (cons (string->list "Erreur de syntaxe: veuillez revoir votre commande.") dict)))
       
       (if (string->number (list->string (car input)))
         ; S'il s'agit d'un nombre.
         ; Enregistrer le nombre dans le stack.
-        (process (cdr input) (append stack (cons (string->number (list->string (car input))) '())))
+        (process (cdr input) (append stack (cons (string->number (list->string (car input))) '())) dict #f)
         
         (if (member (caar input) accepted_operators)
           ; Sinon, s'il s'agit d'un opérateur.
@@ -84,39 +102,49 @@
                           (cadr (reverse stack)) (car (reverse stack)))))
                           
                  (process (cdr input)
-                          (append (remove-last (remove-last stack)) ; Pour retirer les deux derniers éléments du stack.
-                                  (cons result '()))) ; Et ensuite rajouter le résultat calculé.
-            )
+                               (append (remove-last (remove-last stack)) ; Pour retirer les deux derniers éléments du stack.
+                                       (cons result '())) ; Et ensuite rajouter le résultat calculé.
+                               dict
+                               #f))
             
             ; Sinon, il manque un élément pour faire une opération.
-            (string->list "Commande invalide, erreur de syntaxe.")
-          )
+            (cons (string->list "Commande invalide, erreur de syntaxe.") dict))
           
-          (if (eq? (caar input) variable_operator)
-            ; Sinon, si le premier caractère est un opérateur de variable ("=") et que ***TODO : le deuxième caractère est une lettre entre [a-z]***
-            ; Enregistrer le top de la pile dans la variable correspondante (CDAR)... TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            (car (reverse stack))
+          (if (and (eq? (caar input) variable_operator) (= (length (cdar input)) 1) (char<=? #\a (cadar input)) (char>=? #\z (cadar input)))
+            ; Sinon, si le premier caractère est un opérateur de variable ("=") et que le deuxième caractère est une lettre entre [a-z].
+            (if (> (length stack) 0)
+              ; S'il y a une valeur dans le stack, on l'affecte, et on passe à la prochaine commande (s'il y en a) sans toucher au stack.
+              (process (cdr input) stack (affect-var dict (cadar input) (car (reverse stack))) #f)
+              
+              ; S'il n'y a pas de valeur dans le stack à affecter.
+              (cons (string->list "Quelle valeur voulez-vous affecter dans cette commande ?") dict))
             
-            (if (eq? (caar input) "TODO")
+            (if (and (= (length (car input)) 1) (char<=? #\a (caar input)) (char>=? #\z (caar input)))
               ; Sinon, s'il s'agit d'une variable, ajouter son contenu au stack.
-              (string->list "TODOOODODOOOOOOOOO")
+              (if (assoc (caar input) dict)
+                ; Si la variable est affectée, on la met dans le stack et on continue.
+                  (process (cdr input)
+                           (append stack (cdr (assoc (caar input) dict)))
+                           dict
+                           #f)
+                
+                  ; Si la variable n'est pas affectée.
+                  (cons (string->list "Aucune affectation faite pour cette variable.") dict))
               
               ; Si ce n'est rien de tout ça, arrêter tout traitement et retourner une erreur.
-              (string->list "Commande invalide, erreur de syntaxe.")
-            )
-          )
-        )
-      )
-    )
-  )
-)
+              (cons (string->list "Commande invalide, erreur de syntaxe.") dict))))))))
+    
+      (if output?
+        ; Appel final.
+        (cons (append (car return) '(#\newline)) (cdr return))
+        
+        ; Appel interne.
+        return))))
 
 ; La fonction traiter fait appel à "process" en commençant avec un stack vide.
 (define traiter
   (lambda (expr dict)
-    (cons (append (process (split expr char-whitespace?) '()) '(#\newline)) dict)
-  )
-)
+    (process (split expr char-whitespace?) '() dict #t)))
 
 ;;;----------------------------------------------------------------------------
 
